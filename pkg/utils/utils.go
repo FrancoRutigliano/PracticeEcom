32e10efcacd1bbebaa1a46103b482bf416a5a78a
FrancoRutigliano <francorutigliano11@gmail.com>
2024-05-03 10:11:50 -0300

    feat: añadiendo funciones utiles para el programa

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,1 +1,26 @@
 package utils
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func ParseJson(r *http.Request, Payload any) error {
+	if r.Body == nil {
+		return fmt.Errorf("missing request body")
+	}
+
+	return json.NewDecoder(r.Body).Decode(Payload)
+}
+
+func WriteJson(w http.ResponseWriter, status int, v any) error {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	return json.NewEncoder(w).Encode(v)
+}
+
+func WriteErr(w http.ResponseWriter, status int, err error) {
+	WriteJson(w, status, map[string]string{"error": err.Error()})
+}
