9286322b99b15722b33bbd8bc1918d5bb2c3501a
FrancoRutigliano <francorutigliano11@gmail.com>
2024-05-03 20:59:57 -0300

    interface users recibira metodos con los que se interactuara en base de datos + payload users

--- internal/types/types.go
+++ internal/types/types.go
@@ -1,1 +1,18 @@
 package types
+
+import "time"
+
+type UserStore interface {
+	GetUserByEmail(email string) (*User, error)
+	GetUserByID(id int) (*User, error)
+	CreateUser(User) error
+}
+
+type User struct {
+	ID        int       `json:"id"`
+	FistName  string    `json:"firstname"`
+	LastName  string    `json:"lastname"`
+	Email     string    `json:"email"`
+	Password  string    `json:"-"`
+	CreatedAt time.Time `json:"createdAt"`
+}
